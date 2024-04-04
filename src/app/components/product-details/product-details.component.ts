c3491d3625cb373fabd0b0af761fb10dc463a965
GRANELL Marcos <marcos.bacete@ext.soprasteria.com>
2024-04-04 09:30:34 +0200

    create variable

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.scss'],
 })
-export class ProductDetailsComponent {}
+export class ProductDetailsComponent {
+  hola!: string;
+}
