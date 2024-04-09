2561dcb7f4c339bb8d125b925a5de66fb035ee3b
GRANELL Marcos <marcos.bacete@ext.soprasteria.com>
2024-04-09 12:08:16 +0200

    ejercicio4

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../common/products';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product> {
+    return this.http.get<Product>('/assets/data/products.json');
+  }
+}
